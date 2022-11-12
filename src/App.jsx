99e94a401556c6e91d3662c13ab0a0a3757ed72d
Miguel Villanueva <emvil542@gmail.com>
2022-11-12 13:04:08 +0800

    feat(app): add rules modal

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import logo from './logo.svg';
 import './App.scss';
 import { useBEM } from './hooks/useBEM';
 import { useSelector } from 'react-redux';
-import { selectIsModalShow, selectView } from './store/slices/gameSlice';
+import { useAppDispatch } from './hooks/reduxHooks';
+import { selectIsModalShow, selectView, setIsModalShow } from './store/slices/gameSlice';
 import ScoreSect from './components/scoreSect/ScoreSect';
 import Game from './components/game/Game';
 import Results from './components/results/Results';
+import RulesModal from './components/rulesModal/RulesModal';
 
 function App() {
   const [B,E] = useBEM("App")
+  const dispatch = useAppDispatch();
 
   const view = useSelector(selectView);
   const isModalShow = useSelector(selectIsModalShow);
@@ -24,10 +27,12 @@ function App() {
 
   return (
     <div className={B()}>
+      {isModalShow && <RulesModal/>}
       <main className={E('body')}>
         <ScoreSect/>
         {changeView()}
       </main>
+      <button onClick={() => {dispatch(setIsModalShow(true))}} className={E('rules-btn')}>RULES</button>
     </div>
   );
 }
--- src/components/rulesModal/rulesModal.jsx
+++ src/components/rulesModal/rulesModal.jsx
@@ -1,11 +1,20 @@
-import { useBEM } from "../../hooks/useBEM"
+import { useAppDispatch } from "../../hooks/reduxHooks";
+import { useBEM } from "../../hooks/useBEM";
+import { setIsModalShow } from "../../store/slices/gameSlice";
 
 const RulesModal = () => {
     const [B,E] = useBEM('rules')
+    const dispatch = useAppDispatch();
 
     return(
         <div className={B()}>
-
+            <main className={E('cont')}>
+                <header className={E('header')}>
+                    <h2>RULES</h2>
+                    <img onClick={() => {dispatch(setIsModalShow(false))}} className={E('icon')} src="/images/icon-close.svg" alt="" />
+                </header>
+                <img src="/images/image-rules-bonus.svg" alt="rules of the game" />
+            </main>
         </div>
     )
 }
