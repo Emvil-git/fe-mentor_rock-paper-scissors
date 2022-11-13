c897219145fbb9fb5a8b2e4dfb8cda73a2fff7bf
Miguel Villanueva <emvil542@gmail.com>
2022-11-13 15:47:47 +0800

    feat(game): add win conditions

--- src/components/results/Results.jsx
+++ src/components/results/Results.jsx
@@ -1,12 +1,64 @@
 import { useBEM } from "../../hooks/useBEM"
 import { useSelector } from "react-redux"
-import { selectHouseChoice, selectPlayerChoice } from "../../store/slices/gameSlice"
+import { useAppDispatch } from "../../hooks/reduxHooks"
+import { selectHouseChoice, selectPlayerChoice, selectScore, setPlayerChoice, setHouseChoice } from "../../store/slices/gameSlice"
 import GameBtn from "../game/gameButton/GameButton"
 
 const Results = () => {
     const [B,E] = useBEM('results')
+
+    const dispatch = useAppDispatch()
+
     const player = useSelector(selectPlayerChoice)
     const house = useSelector(selectHouseChoice)
+
+    const condArr = [
+        {
+            val: 'rock',
+            win : ['scissors', 'lizard'],
+            lose : ['spock', 'paper'],
+        },
+        {
+            val: 'paper',
+            win : ['rock', 'spock'],
+            lose : ['scissors', 'lizard'],
+        },
+        {
+            val: 'scissors',
+            win : ['paper', 'lizard'],
+            lose : ['spock', 'rock'],
+        },
+        {
+            val: 'lizard',
+            win : ['paper', 'spock'],
+            lose : ['rock', 'scissors'],
+        },
+        {
+            val: 'spock',
+            win : ['scissors', 'rock'],
+            lose : ['paper', 'lizard'],
+        },
+    ]
+
+    const choiceCond = condArr.find(cond => cond.val === player);
+
+    const results = () => {
+        switch(true){
+            case player === house:
+                return "DRAW!"
+            case choiceCond.win.includes(house):
+                return "YOU WIN"
+            case choiceCond.lose.includes(house):
+                return "YOU LOSE"
+        }
+    }
+
+    const again = () => {
+        dispatch(setHouseChoice(undefined))
+        dispatch(setPlayerChoice(undefined))
+    }
+
+    console.log(choiceCond)
 
     return(
         <div className={B()}>
@@ -16,8 +68,8 @@ const Results = () => {
                     <GameBtn type={player} isStatic={true}/>
                 </section>
                 <section className={E('res')}>
-                    <span className={E('res-label')}>YOU LOSE</span>
-                    <button className={E('res-btn')}>PLAY AGAIN</button>
+                    <span className={E('res-label')}>{results()}</span>
+                    <button onClick={again} className={E('res-btn')}>PLAY AGAIN</button>
                 </section>
                 <section className={E('choice')}>
                     <h3 className={E('choice-label')}>THE HOUSE PICKED</h3>
